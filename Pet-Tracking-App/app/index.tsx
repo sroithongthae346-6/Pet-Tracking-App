b7789372939c84476fd7c1ae6ef09bd63eb14e3a
sroithongthae346-6 <sroithongthae.a@kkumail.com>
2025-08-16 02:27:05 +0700

    update

--- Pet-Tracking-App/app/index.tsx
+++ Pet-Tracking-App/app/index.tsx
@@ -1,10 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      className="flex-1 items-center justify-center items-center">
+    <View className="flex-1 items-center justify-center items-center">
       <Text className="text-2xl text-primary">Hay Welcome!</Text>
+      <Link href="/onboarding">
+        <Text className="text-lg text-secondary">Get Started</Text>
+      </Link>
     </View>
   );
 }
---
+++ Pet-Tracking-App/app/onboarding.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet,Text, View } from "react-native";
+import Ract from "react";
+
+const Onboarding = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-2xl text-primary">Welcome to the App!</Text>
+    </View>
+  );
+};
+
+export default Onboarding;
